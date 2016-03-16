6f528299f90c385dd9b32170b43150de35a68381
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-16 14:09:24 +0100

    Fixed License field

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
-#include <cassert>
-#include <iostream>
 #include "do_magic_cpp.h"
 
 int main()
 {
-  assert(do_magic_cpp(2) == 4);
-  std::cout << do_magic_cpp(42) << '\n';
+  if (do_magic_cpp(2) != 4) return 1;
 }
