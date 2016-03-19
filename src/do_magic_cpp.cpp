52af3bb19e1b0a7bb44656f960df0d95e21c5d47
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-19 14:20:03 +0100

    Travis: can I remove this?

--- src/do_magic_cpp.cpp
+++ src/do_magic_cpp.cpp
@@ -1,18 +1,5 @@
 #include "do_magic_cpp.h"
 
-
-// When you get:
-// error: Rcpp.h: No such file or directory
-// then use
-//
-// cd /
-// find . | egrep "Rcpp\.h"
-//
-// and add the path to the INCLUDEPATH
-#include <Rcpp.h>
-
-using namespace Rcpp;
-
 //' Does magic
 //' @param x Input
 //' @return Magic value
@@ -22,6 +9,3 @@ int do_magic_cpp(const int x)
 {
   return x * 2;
 }
-
-//' @useDynLib domagic
-//' @importFrom Rcpp sourceCpp
